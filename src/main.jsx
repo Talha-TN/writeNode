b1928327e1a5c6cae4b774fa6ad619fb64d61929
Talha-TN <nawazt964@gmail.com>
2025-01-23 14:38:44 -0800

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import "react-toastify/dist/ReactToastify.css";
 import store from "./store/store.jsx";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App.jsx";   
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
