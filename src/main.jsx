f54b1524112ec66a279dec751ba8bef417551d20
Talha-TN <nawazt964@gmail.com>
2025-01-23 10:41:25 -0800

    solve the bug of disappering post when redirect to page not found and then visit home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import store from './store/store.jsx'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
+import store from "./store/store.jsx";
+import { persistor } from "./store/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-    <App />
-    <ToastContainer
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+        <ToastContainer
           position="top-right"
           autoClose={2270}
           hideProgressBar={false}
@@ -24,7 +28,7 @@ createRoot(document.getElementById('root')).render(
           pauseOnHover
           theme="dark"
         />
-    </Provider>
+      </Provider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
